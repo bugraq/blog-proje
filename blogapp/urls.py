703b3fb5baf7bf9722dda8958c954264d5e650e2
Fırat Enformatik <firatenformatik@Frat-iMac.local>
2025-05-07 16:08:25 +0300

    gerekli kodlar eklendi templates urls admin.py ...

---
+++ blogapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('add/', views.add_post, name='add_post'),
+]
